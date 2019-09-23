ca773d2b640d49df2b4dc404e1ccaf85c0ab9e5a
Yaroslav <garedrag@gmail.com>
2019-09-23 20:50:09 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-yum install -y gcc-c++ make
+sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
-yum -y install nodejs
-yum -y install git
-yum -y install mc
+sudo yum -y install nodejs
+sudo yum -y install git
+sudo yum -y install mc
 sudo git clone https://github.com/garedrag/chess.git
 cd /chess
-npm install
-npm build
-npm run build
+sudo npm install
+sudo npm build
+sudo npm run build
 npm start & sudo npm run client
