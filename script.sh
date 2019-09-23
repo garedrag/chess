9ba0a373bb73fff693e68aeb805f4c5731201017
Yaroslav <garedrag@gmail.com>
2019-09-23 20:30:51 +0300

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+sudo yum -y install nodejs
+sudo yum -y install git
+sudo yum -y install mc
+sudo git clone https://github.com/garedrag/chess.git
+cd /chess
+sudo npm install
+npm build
+sudo npm run build
+npm start & sudo npm run client
