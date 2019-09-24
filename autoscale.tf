e6fc5b9dc3ddca134c6b5701731c50a8fd8c10c0
Yaroslav <garedrag@gmail.com>
2019-09-24 10:41:22 +0300

    Update autoscale.tf

--- autoscale.tf
+++ autoscale.tf
@@ -29,7 +29,7 @@ resource "aws_instance" "web" {
 
 ### Creating Security Group for EC2
 resource "aws_security_group" "instance" {
-  name = "gare-terraform-example-instance-Trevis2"
+  name = "gare-terraform-example-instance-Trev"
   ingress {
     from_port   = 8080
     to_port     = 8080
@@ -144,7 +144,7 @@ resource "aws_security_group" "elb" {
 }
 ### Creating ELB
 resource "aws_elb" "example" {
-  name               = "terraform-asg-example-Trevis"
+  name               = "terraform-asg-example-Trev"
   security_groups    = ["${aws_security_group.elb.id}"]
   availability_zones = "${data.aws_availability_zones.all.names}"
   health_check {
