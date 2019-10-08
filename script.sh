28fb8a98ff4fabc099197390abed172fdf796a17
Yaroslav <garedrag@gmail.com>
2019-10-08 19:06:48 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-sudo yum install -y gcc-c++ make
+sudo apt-get install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_4.x | sudo -E bash -
-sudo yum -y install nodejs
-sudo yum -y install git
-sudo yum -y install mc
+sudo apt-get -y install nodejs
+sudo apt-get -y install git
+sudo apt-get -y install mc
 sudo git clone https://github.com/garedrag/chess.git
 sed -i "s/localhost/$(echo "$(hostname -I)" | sed 's/ $//')/g" /chess/lib/client/assets/nodejs-chess-0.1.0.js
 sed -i "s/localhost/$(echo "$(hostname -I)" | sed 's/ $//')/g" /chess/config.js
